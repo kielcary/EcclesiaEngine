28ff1e8f09ce4219fa8926cc3ae9ad8dede9079a
Kiel Cary <kielcary@gmail.com>
2019-04-09 20:37:52 -0500

    Adding solution files

---
+++ EcclesiaEngine/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EcclesiaEngine.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
